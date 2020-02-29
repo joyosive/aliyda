0a6991da7a850d3b2e2dda8e9a58c292a3664c05
JoyOsive <49386431+joyosive@users.noreply.github.com>
2020-02-29 16:07:34 +0530

    Update WebSecurityConfig.java

--- gdprUIKafkaFabricApp/src/main/java/com/gdprapp/configs/WebSecurityConfig.java
+++ gdprUIKafkaFabricApp/src/main/java/com/gdprapp/configs/WebSecurityConfig.java
@@ -30,17 +30,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/resources/**", "/register").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
+       http
+               .authorizeRequests()
+               .antMatchers("/resources/**", "/register").permitAll()
+               .anyRequest().authenticated()
+               .and()
+               .formLogin()
+               .loginPage("/login")
+               .permitAll()
+               .and()
+               .logout()
+               .permitAll();
 
         http.httpBasic().disable();
     }
