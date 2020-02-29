a3306d22f0aa06f68caca353ff77215177b75891
JoyOsive <49386431+joyosive@users.noreply.github.com>
2020-02-29 16:10:35 +0530

    Update KafkaController.java

--- gdprUIKafkaFabricApp/src/main/java/com/gdprapp/kafka/KafkaController.java
+++ gdprUIKafkaFabricApp/src/main/java/com/gdprapp/kafka/KafkaController.java
@@ -1,33 +1,33 @@
 package com.gdprapp.kafka;
 
-//
-//import com.gdprapp.kafka.bo.TaskBO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Date;
-//
-//@RestController
-//@RequestMapping("kafka")
-//public class KafkaController {
-//
-//    @Autowired
-//    KafkaTemplate<String, TaskBO> stringTaskBOKafkaTemplate;
-//
-//    public static final String TOPIC_TASK = "kafka_tasks";
-//
-//    @GetMapping("/publish/json/{name}")
-//    public String postJson(@PathVariable("name") final String name) {
-//
-//        TaskBO taskBO = new TaskBO(name,new Date());
-//        stringTaskBOKafkaTemplate.send(TOPIC_TASK, taskBO);
-//
-//        return "Published successfully";
-//    }
-//
-//}
+
+import com.gdprapp.kafka.bo.TaskBO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("kafka")
+public class KafkaController {
+
+   @Autowired
+   KafkaTemplate<String, TaskBO> stringTaskBOKafkaTemplate;
+
+   public static final String TOPIC_TASK = "kafka_tasks";
+
+   @GetMapping("/publish/json/{name}")
+   public String postJson(@PathVariable("name") final String name) {
+
+       TaskBO taskBO = new TaskBO(name,new Date());
+       stringTaskBOKafkaTemplate.send(TOPIC_TASK, taskBO);
+
+       return "Published successfully";
+   }
+
+}
